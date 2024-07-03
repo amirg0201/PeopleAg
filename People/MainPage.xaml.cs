20f77ab1a97f327927c6041fe1ff7647bfd61a2c
amirg0201 <amir.garcia@udla.edu.ec>
2024-07-03 11:11:53 -0500

    Agregar archivos de proyecto.

---
+++ People/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using People.Models;
+using System.Collections.Generic;
+
+namespace People;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    public void OnNewButtonClicked(object sender, EventArgs args)
+    {
+        statusMessage.Text = "";
+
+        App.PersonRepoAg.AddNewPerson(newPerson.Text);
+        statusMessage.Text = App.PersonRepoAg.StatusMessage;
+    }
+
+    public void OnGetButtonClicked(object sender, EventArgs args)
+    {
+        statusMessage.Text = "";
+
+        List<PersonAg> people = App.PersonRepoAg.GetAllPeople();
+        peopleList.ItemsSource = people;
+    }
+
+}
+
---
+++ People/Models/PersonAg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace People.Models
+{
+    [Table("people")]
+    public class PersonAg
+    {
+        [PrimaryKey, AutoIncrement]
+        public int AgId { get; set; }
+
+        [MaxLength(250), Unique]
+        public string NameAg { get; set; }
+    }
+}
